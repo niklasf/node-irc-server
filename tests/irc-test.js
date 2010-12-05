d699c687e215bdfaaa888904d7a114237fe6a985
Niklas Fiekas <niklas.fiekas@googlemail.com>
2010-12-05 21:11:04 +0100

    Don't require event manager capabilities for the server itself

--- tests/irc-test.js
+++ tests/irc-test.js
@@ -1,23 +1,5 @@
 var irc = require('../lib/irc');
 
-exports["test as event manager capabilities"] = function(test) {
-	test.expect(2);
-
-	var server = irc.createServer();
-	
-	server.on('event', function() {
-		test.ok(true, 'event occured');
-	});
-	
-	server.default('event', function() {
-		test.ok(true, 'default implementation called');
-	});
-	
-	server.emit('event');
-	
-	test.done();
-};
-
 exports["test as network server"] = function(test) {
 	test.expect(0);
 	
