3caa9d7314357075438f745140fb21608a41d8bc
Niklas Fiekas <niklas.fiekas@googlemail.com>
2010-12-06 16:43:23 +0100

    Added test for PING

--- tests/irc-test.js
+++ tests/irc-test.js
@@ -1,8 +1,30 @@
-var irc = require('../lib/irc');
+var irc = require('../lib/irc'),
+	EventEmitter = require('events').EventEmitter;
 
 exports["test as network server"] = function(test) {
 	test.expect(0);
 	
 	var server = irc.createServer();
 	server.listen(7834, '127.0.0.1', test.done);
+};
+
+exports["ping the server"] = function(test) {
+	test.expect(4);
+
+	var server = irc.createServer();
+	
+	var client = new EventEmitter();
+	
+	client.deliver = function(from, to, command, args) {
+		test.equals(from, server.name, 'back from server');
+		test.equals(to, server.name, 'Back to the server');
+		test.equals(command, 'PONG', 'sent PONG');
+		test.equals(args, ['test']);
+	};
+	
+	server.addClient(client);
+	
+	client.emit('message', 'PING :test');
+	
+	test.done();
 };
