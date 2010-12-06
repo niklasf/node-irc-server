ee4422a7e89ab3e7c5af0d6bf82707c3c2b249bf
Niklas Fiekas <niklas.fiekas@googlemail.com>
2010-12-06 17:29:49 +0100

    Added tools.unixtime()

--- tests/tools-test.js
+++ tests/tools-test.js
@@ -19,3 +19,10 @@ exports["test object merging"] = function(test) {
 	
 	test.done();
 };
+
+exports["get unixtime"] = function(test) {
+	test.expect(2);
+	test.ok(tools.unixtime() > 1291590000, 'december 06 in 2010 is over');
+	test.ok(tools.unixtime() < 2000000000, 'were not *that* far in the future');
+	test.done();
+};
