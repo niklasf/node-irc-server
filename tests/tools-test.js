0b2e844a7042af85df7bbcfbc65cac979f7852d1
Niklas Fiekas <niklas.fiekas@googlemail.com>
2010-12-07 21:23:22 +0100

    Test for the yet to implement Array.delete and Array.contains

--- tests/tools-test.js
+++ tests/tools-test.js
@@ -26,3 +26,45 @@ exports["get unixtime"] = function(test) {
 	test.ok(tools.unixtime() < 2000000000, 'were not *that* far in the future');
 	test.done();
 };
+
+exports["array contains"] = function(test) {
+	test.expect(5 + 4 + 2 + 2);
+
+	var one = [1, 2, 3, 4, 5];
+	var two = ['one', 'two', 'three', 'four', 'five'];
+	
+	test.ok(one.contains(2));
+	test.ok(one.contains(4, 5));
+	test.ok(one.contains(5, 4));
+	test.ok(one.contains(4, 4));
+	test.ok(one.contains(1, 2, 3, 4, 5));
+	
+	test.ok(!one.contains('1'), 'strict equal');
+	test.ok(!one.contains(one));
+	test.ok(!one.contains('12'));
+	test.ok(!one.contains(1, 2, 3, 100));
+	
+	test.ok(two.contains('two'));
+	test.ok(two.contains('four', 'one'));
+	
+	test.ok(!two.contains('two', 'three', 'anything'));
+	test.ok(!two.contains(two));
+	
+	test.done();
+};
+
+exports["array delete"] = function(test) {
+	test.expect(5 + 1);
+	
+	var array = ['one', 'two', 'three', 1, 2, 3];
+	
+	test.deepEqual(array.remove('one'), ['two', 'three', 1, 2, 3]);
+	test.deepEqual(array.remove('one', 'two'), ['three', 1, 2, 3]);
+	test.deepEqual(array.remove('nothing', 'really not'), array);
+	test.deepEqual(array.remove(1, 2, 3), ['one', 'two', 'three']);
+	test.deepEqual(array.remove('three', 'nothing'), ['one', 'two', 1, 2, 3]);
+	
+	test.deepEqual(array, ['one', 'two', 'three', 1, 2, 3]);
+	
+	test.done();
+};
